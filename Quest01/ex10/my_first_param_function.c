7d361cd14f273344fa2f201eed48c47bcd02fd0f
khalilmasri <33801628+khalilmasri@users.noreply.github.com>
2021-07-18 13:45:21 +0300

    my_first_param_function.c

---
+++ Quest01/ex10/my_first_param_function.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+// function will 
+
+int main() {
+  int timer = 10;
+
+  while (timer > 0) {
+    printf("detonation in... %d secondes.\n", timer);
+    timer--;
+  }
+  return 0;
+}
